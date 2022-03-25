038991dbe183d3e3ec4734cfd147f27dec8f9e4c
Jhorman <jhorman.arias.9@gmail.com>
2022-03-24 23:28:47 -0500

    first version component Navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='../images/logoOpentify.png' width='200' height='200' />
+        <br />
+        <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
