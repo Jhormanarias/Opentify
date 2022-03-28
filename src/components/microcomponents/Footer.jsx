532ac92f1b28ff28b5409bdc6816304f73992f1d
Jhorman <jhorman.arias.9@gmail.com>
2022-03-27 23:33:44 -0500

    Styles body

--- src/components/microcomponents/Footer.jsx
+++ src/components/microcomponents/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ()=> {
     return(
-        <div className="container-fluid mt-3 text-light bg-dark p-5">
+        <div className="container-fluid fixed-bottom mt-3 text-light bg-dark p-5">
             <span> GañanTech © 2022 </span>
         </div>
     );
